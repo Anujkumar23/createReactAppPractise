1ddaf1956d5e93d60bb2c84d6ad1939f843428b5
anuj <anujthakur315@gmail.com>
2022-11-09 10:41:27 +0530

    made a KittyGallery with useref

---
+++ useref/src/KittyGallery.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useRef } from "react";
+
+
+function KittyGallery() {
+  const galleryRef = useRef(null);
+  const index = useRef(0);
+  function showNextimg() {
+    let imageList = galleryRef.current.querySelectorAll(".image");
+    index.current = index.current + 1 >= imageList.length ? 0 : index.current + 1;
+      imageList[index.current].scrollIntoView({
+        behaviour:"smooth",
+        block:"nearest",
+        inline:"center"
+
+
+      })
+  }
+
+  return (
+    <div>
+      <h1>KittyGallery</h1>
+      <button onClick={showNextimg}>Next</button>
+      <ul ref={galleryRef} className="gallery">
+        <li className="image" >
+          <img src="http://placekitten.com/g/200/300" alt="" />
+        </li>
+        <li className="image">
+          <img src="http://placekitten.com/g/200/250" alt="" />
+        </li>
+        <li className="image" >
+          <img src="http://placekitten.com/g/300/300" alt="" />
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default KittyGallery;
