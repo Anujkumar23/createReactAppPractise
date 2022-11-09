a8d14588f29da7c6042f7c4a0886b96216e6dafd
anuj <anujthakur315@gmail.com>
2022-11-09 10:50:35 +0530

    ckeck position of cursor with useEffect

--- useeffect/src/CursorPosition.jsx
+++ useeffect/src/CursorPosition.jsx
@@ -1,34 +1,28 @@
-import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
-function UseEffectExample() {
-    console.log(" render")
+function CursorPosition() {
+  console.log(" render");
 
-    const [cursorPosition, setCursorPosition] = useState({x:0,y:0})
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
 
-    useEffect(() => {
-        console.log("addevent listener added")
+  useEffect(() => {
+    console.log("addevent listener added");
 
-        const handleCursor=(e)=>{
-            setCursorPosition({x:e.clientX,y:e.clientY})
-            console.log("pointer move event")
-           }
-        
-        window.addEventListener("pointermove",handleCursor)
-       
-    
-      
-    }, [])
-    
+    const handleCursor = (e) => {
+      setCursorPosition({ x: e.clientX, y: e.clientY });
+      console.log("pointer move event");
+    };
 
-  
-      
-
+    window.addEventListener("pointermove", handleCursor);
+  }, []);
 
   return (
-        <div>x:{cursorPosition.x},y:{cursorPosition.y}</div>
-  )
-} 
+    <div>
+      x:{cursorPosition.x},y:{cursorPosition.y}
+    </div>
+  );
+}
 
-export default UseEffectExample
+export default CursorPosition;
