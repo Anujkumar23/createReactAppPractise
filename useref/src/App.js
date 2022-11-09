392fdb091e4747fc015eddedfd32af80de885023
anuj <anujthakur315@gmail.com>
2022-11-09 10:52:56 +0530

    add a component

--- useref/src/App.js
+++ useref/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Stopwatch from './Stopwatch';
+import KittyGallery from './KittyGallery';
 
 function App() {
   return (
     <div className="App">
       {/* <Counter/> */}
-      <Stopwatch/>
+      {/* <Stopwatch/> */}
+      <KittyGallery/>
     </div>
   );
 }
