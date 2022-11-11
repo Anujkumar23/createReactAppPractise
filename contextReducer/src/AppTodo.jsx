a7685782e6b0cdd81938b80ad6baae7ba58ecec5
anuj <anujthakur315@gmail.com>
2022-11-11 20:00:21 +0530

    return the component

--- contextReducer/src/AppTodo.jsx
+++ contextReducer/src/AppTodo.jsx
@@ -1,38 +1,23 @@
 import React from "react";
-import { useReducer } from "react";
-import TodoReducer  from "./TodoReducer";
 import AddTodo from "./AddTodo";
+import CompletedTodos from "./CompletedTodos";
 import TodoList from "./TodoList";
-import { createContext } from "react";
+import TodosProvider from "./TodosContext"
 
 
-const initialTodos = [
-  { id: 1, text: "Learn javaScript", done: true },
-  { id: 2, text: "Learn React", done: false },
-  { id: 3, text: "Learn Node", done: false },
-];
 
-export const TodosContext=createContext(null)
-export const TodosDispatchContext=createContext(null)
+
 
 function AppTodo() {
-  const [todos, dispatch] = useReducer(TodoReducer, initialTodos);
-
-
+  
     return (
-      <div className="main">
-        <TodosContext.Provider value={todos}>
-          <TodosDispatchContext value ={dispatch}>
-        <h1>Todos</h1>
-        <AddTodo />
-        <TodoList
-          todos={todos}
-        />
-        
-          </TodosDispatchContext>
-        </TodosContext.Provider>
-       
-      </div>
+     <TodosProvider>
+      <h1>Todos</h1>
+      <AddTodo/>
+      <TodoList/>
+      <CompletedTodos/>
+     </TodosProvider>
+
     );
   }
 
