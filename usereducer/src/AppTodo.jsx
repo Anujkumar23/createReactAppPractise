f1b1c2a6ae1be85ad2b799f880608f16a04979bc
anuj <anujthakur315@gmail.com>
2022-11-11 12:20:01 +0530

    add AppTodo component

---
+++ usereducer/src/AppTodo.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { useReducer } from "react";
+import todoReducer from "./TodoReducer";
+import AddTodo from "./AddTodo";
+import TodoList from "./TodoList";
+
+let nextId = 4;
+const initialTodos = [
+  { id: 1, text: "Learn javaScript", done: true },
+  { id: 2, text: "Learn React", done: false },
+  { id: 3, text: "Learn Node", done: false },
+];
+
+
+
+function AppTodo() {
+  const [todos, dispatch] = useReducer(todoReducer, initialTodos);
+
+  function handleAddTodo(text) {
+    dispatch({
+      type: "add",
+      id: nextId++,
+      text,
+    });
+}
+
+    function handleTodoChange(updatedTodo) {
+      dispatch({
+        type: "change",
+        todo: updatedTodo,
+      });
+    }
+
+    function handleTodoDelete(todoId) {
+      dispatch({
+        type: "remove",
+        id: todoId,
+      });
+    }
+    return (
+      <div className="main">
+        <h1>Todos</h1>
+        <AddTodo onAddTodo={handleAddTodo} />
+        <TodoList
+          todos={todos}
+          onTodoChange={handleTodoChange}
+          onTodoDelete={handleTodoDelete}
+        />
+      </div>
+    );
+  }
+
+
+export default AppTodo;
